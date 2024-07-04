3bd9ff3a0dd760ec5559c47a1a30c26c52d72efc
eliams92 <110405311+eliams92@users.noreply.github.com>
2024-07-04 22:16:34 +0530

    Update bulk_conductance.py

--- bulk_conductance.py
+++ bulk_conductance.py
@@ -11,7 +11,9 @@ import matplotlib as mpl
 from matplotlib import pyplot as plt
 
 
-def qsh_system(a, t=1.0, W=100, r1=80, r2=200):
+def qsh_system(a, t=1.0, W=100, r1=80, r2=200): 
+    #The function converts the Hamiltoninan written in momentum space to real space with square lattice with lattice constant 'a' with commands at line 32 and line 33. 
+    #The parameters C, B, A, M, Dz, D1R, D1I, D2R, D2I become variables at this stage and can be given values when calculations need to be done.
     
     hamiltonian = """
     
@@ -30,11 +32,14 @@ def qsh_system(a, t=1.0, W=100, r1=80, r2=200):
     template = kwant.continuum.discretize(hamiltonian, grid=a)
     lat = kwant.lattice.square(a)
 
+    #The shape function defines the geometry of the real-space lattice. Only the spatial coordinates that are between 'r1' and 'r2' are considered (see line 39). 
     def shape(site):
         (x, y) = site.pos
         rsq = x**2 + y**2
-        return (r1**2 < rsq < r2**2)
-
+        return (r1**2 <= rsq <= r2**2)
+        
+    #Leads are connected to measure bulk conductances in a Corbino disc. It is an infinitely long rectangular-shaped lead with a width 'W'. 
+    #A lead is built from infinity to the scattering region i.e. outer circle of the Corbino disc.
     def lead_shape(site):
         (x, y) = site.pos
         return (-W/2 < y < W/2)
@@ -42,6 +47,7 @@ def qsh_system(a, t=1.0, W=100, r1=80, r2=200):
     syst = kwant.Builder()
     syst.fill(template, shape, (0, r1+a))
     
+    #lead and lead1 are leads that are bult from positive and negative infinity to be attached to the Corbino disc 
     lead = kwant.Builder(kwant.TranslationalSymmetry([a, 0]))
     lead.fill(template, lead_shape, (0, 0))
     lead1 = kwant.Builder(kwant.TranslationalSymmetry([-a,0]))
@@ -56,13 +62,13 @@ def qsh_system(a, t=1.0, W=100, r1=80, r2=200):
 def analyze_qsh(a, eg,d1r,d1i,d2r,d2i):
 
     a2= a*a
-   
+    #A dictionary of values for the parameters.
     params = dict(A=0.06*a, B=0.5*a2, D=0, M=eg, C=0, Dz=0.02, D2R=d2r*a, D1R=d1r, D2I=d2i*a, D1I=d1i)
-    syst=qsh_system(a=a)
+    syst=qsh_system(a=a) 
 
     
 
-
+    #data is a list that stores the bulk conductance for each value in the array 'energies'
     data =[]
     energies = np.linspace(-0.20,0.20,320)
     
@@ -76,7 +82,7 @@ def analyze_qsh(a, eg,d1r,d1i,d2r,d2i):
         data.append(smatrix.transmission(1, 0))
         #file.write("%f\n" %energy)
 
-    #saves the generated bulkconductance as a function of energies in the following .dat file
+    #saves the generated bulk conductance as a function of energy in energies in the following .dat file
     fname="bulk_cond"+str(params['M'])
     thefile = open(fname + ".dat","w")
     for i in range(len(data)):
@@ -86,11 +92,11 @@ def analyze_qsh(a, eg,d1r,d1i,d2r,d2i):
 def main():
 
     aa = 1
-
+    #takes the values from the file as a table (two-dimensional array).
     fl=np.loadtxt('Eg_input.txt')
-
+    #used to loop Eg in shell script to submit jobs in HPC
     gl=np.loadtxt('input.dat', comments='!')
-
+    
     iEg = int(gl)
    
     for i in range(iEg-1,iEg):
